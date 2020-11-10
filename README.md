da6b8102ce013733e6a7f155bc5db7abf7704f4d
bartektelec <bartektelec@gmail.com>
2020-11-10 19:03:41 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Foodie
+
+a Restaurant search app built in plain JavaScript
+This was an assignment for Noroff's Front-end 2nd Year course.
+We weren't allowed to use any external frameworks or libraries for the front-end so I made my own reactive view library inspired by hyperapp.
+
+[LIVE DEMO](http://bartektelec.github.io/foodie-app/)
+
+- Bootstrap based UI
+- Strapi as a back-end
+- the Reactive "state" uses the Observer pattern to notify all the bound elements when it's changed
+- Reactive Search bar, search for restaurant names as well as food types
+- Add restaurants to favourites
+- View the menu and add meals to cart
+- Cart shows all added items and sums up the total price
+- Favourite restaurants and shopping cart are stored in LocalStorage
+- Authorization wasn't a goal in this assignment so I didn't focus on that
+
+## Credits
+
+- [Bootstrap v4.5](https://getbootstrap.com/)
+- [Pexels](https://www.pexels.com/nb-no/)
+- [Eva-Icons](https://akveo.github.io/eva-icons/#/)
+- [Strapi](https://strapi.io/)
--- js/components/login.js
+++ js/components/login.js
@@ -1,18 +1,18 @@
-import Component from "../core/Component.js";
-import Reactive from "../core/Reactive.js";
-import { navigateTo } from "../app.js";
-import { API_URL } from "../vars.js";
-import { addAlert } from "./alert.js";
-import { toggleModal } from "./modal.js";
-import { jwt } from "../models/index.js";
+import Component from '../core/Component.js';
+import Reactive from '../core/Reactive.js';
+import { navigateTo } from '../app.js';
+import { API_URL } from '../vars.js';
+import { addAlert } from './alert.js';
+import { toggleModal } from './modal.js';
+import { jwt } from '../models/index.js';
 
 const loginUser = async () => {
   // alert(`${username.value} ${password.value}`);
   try {
-    const req = await fetch(API_URL + "/auth/local/", {
-      method: "POST",
+    const req = await fetch(API_URL + '/auth/local/', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         identifier: username.value,
@@ -21,49 +21,49 @@ const loginUser = async () => {
     });
     const data = await req.json();
     isLoggedIn.value = true;
-    jwt.set(data.jwt);
+    // jwt.set(data.jwt);
     toggleModal();
-    navigateTo("login");
+    navigateTo('login');
   } catch (e) {
     isLoggedIn.value = false;
-    addAlert(`💩 ${e.message}`, "danger");
+    addAlert(`💩 ${e.message}`, 'danger');
   }
 };
 
-const username = Reactive("");
-const password = Reactive("");
+const username = Reactive('');
+const password = Reactive('');
 
 export const isLoggedIn = Reactive(jwt.isSet());
 
-const login = new Component("div", { class: "card modal-card" }, [
-  new Component("div", { class: "card-header" }, "Log in"),
-  new Component("div", { class: "card-body" }, [
-    new Component("div", { class: "form-group" }, [
-      new Component("label", {}, "Username"),
-      new Component("input", { class: "form-control" })
-        .attr("type", "text")
+const login = new Component('div', { class: 'card modal-card' }, [
+  new Component('div', { class: 'card-header' }, 'Log in'),
+  new Component('div', { class: 'card-body' }, [
+    new Component('div', { class: 'form-group' }, [
+      new Component('label', {}, 'Username'),
+      new Component('input', { class: 'form-control' })
+        .attr('type', 'text')
         .model(username),
     ]),
-    new Component("div", { class: "form-group" }, [
-      new Component("label", {}, "Password"),
-      new Component("input", { class: "form-control" })
-        .attr("type", "password")
+    new Component('div', { class: 'form-group' }, [
+      new Component('label', {}, 'Password'),
+      new Component('input', { class: 'form-control' })
+        .attr('type', 'password')
         .model(password),
     ]),
     new Component(
-      "a",
-      { attr: ["href", "#"], on: ["click", () => toggleModal("register")] },
-      "Create a new account"
+      'a',
+      { attr: ['href', '#'], on: ['click', () => toggleModal('register')] },
+      'Create a new account'
     ),
   ]),
-  new Component("div", { class: "card-footer" }, [
+  new Component('div', { class: 'card-footer' }, [
     new Component(
-      "button",
-      { class: "btn btn-outline-danger" },
-      "Close"
-    ).on("click", () => toggleModal()),
-    new Component("button", { class: "btn btn-primary" }, "Log in").on(
-      "click",
+      'button',
+      { class: 'btn btn-outline-danger' },
+      'Close'
+    ).on('click', () => toggleModal()),
+    new Component('button', { class: 'btn btn-primary' }, 'Log in').on(
+      'click',
       () => loginUser()
     ),
   ]),
